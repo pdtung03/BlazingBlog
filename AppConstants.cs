1d64967c9ccbc9dcdfa69d1eac0ad483a7bcb616
pdtung03 <TungPDHE173492@fpt.edu.vn>
2025-03-13 20:39:51 +0700

    modify login page,add admin dashboard

---
+++ AppConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazingBlog
+{
+    public static class AppConstants
+    {
+        public static class ClaimNames
+        {
+            public const string FullName = "FullName";
+        }
+
+    }
+}
---
+++ Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace BlazingBlog
+{
+    public static class Extensions
+    {
+        public static string GetUserName(this ClaimsPrincipal principal) =>
+            principal.FindFirstValue(AppConstants.ClaimNames.FullName)!;
+        public static string GetUserId(this ClaimsPrincipal principal) =>
+            principal.FindFirstValue(ClaimTypes.NameIdentifier)!;
+    }
+}
